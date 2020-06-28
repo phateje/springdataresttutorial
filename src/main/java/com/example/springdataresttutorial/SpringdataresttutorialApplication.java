6d2eb0b95ce05a233721c92b55d9e1c0d71954e8
Phate Phate <phate1986@hotmail.com>
2020-06-28 14:24:23 -0700

    first commit

---
+++ src/main/java/com/example/springdataresttutorial/SpringdataresttutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdataresttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataresttutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdataresttutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdataresttutorial/SpringdataresttutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdataresttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataresttutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
